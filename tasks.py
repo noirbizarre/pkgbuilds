42819269e17a34c2f0d16b89912f553bfcea04fd
Axel Haustant <noirbizarre@gmail.com>
2020-06-24 16:44:43 +0200

    Invoke skeleton

---
+++ tasks.py
@@ -0,0 +1,77 @@
+'''
+System, shell & invoke related helpers
+'''
+import os
+import sys
+
+from pathlib import Path
+from invoke import task
+
+
+#: Project absolute root path
+ROOT = Path(__file__).parent
+CWD = Path(os.getcwd()})
+
+
+def color(code):
+    '''A simple ANSI color wrapper factory'''
+    return lambda t: '\033[{0}{1}\033[0;m'.format(code, t)
+
+
+green = color('1;32m')
+red = color('1;31m')
+cyan = color('1;36m')
+white = color('1;39m')
+yellow = color('1;33m')
+
+
+def header(text, *args, **kwargs):
+    '''Display an header'''
+    text = text.format(*args, **kwargs)
+    print(' '.join((yellow('★'), white(text), yellow('★'))))
+    sys.stdout.flush()
+
+
+def info(text, *args, **kwargs):
+    '''Display informations'''
+    text = text.format(*args, **kwargs)
+    print(' '.join((cyan('➤'), text)))
+    sys.stdout.flush()
+
+
+def success(text, *args, **kwargs):
+    '''Display a success message'''
+    text = text.format(*args, **kwargs)
+    print(' '.join((green('✔'), white(text))))
+    sys.stdout.flush()
+
+
+def warn(text, *args, **kwargs):
+    '''Display a warning message'''
+    text = text.format(*args, **kwargs)
+    print(' '.join((yellow('⚠'), white(text))))
+    sys.stdout.flush()
+
+
+def error(text, *args, **kwargs):
+    '''Display an error message'''
+    text = text.format(*args, **kwargs)
+    print(' '.join((red('✘'), yellow(text))))
+    sys.stdout.flush()
+
+
+def exit(text=None, code=-1):
+    if text:
+        error(text)
+    sys.exit(-1)
+
+
+@task
+def debug(ctx):
+    header('header')
+    info('info')
+    success('success')
+    warn('warning')
+    error('error')
+    info(f'ROOT: {ROOT}')
+    info(f'CWD: {CWD}')
